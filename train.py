407983b1875552f239f78cb180a09d33b1cddaf9
25095 <2509562097@qq.com>
2024-08-03 13:52:03 +0800

    update

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import torch
 
 def train_p1(net, mi_model, X, valid_lens, opt, scaler):
     opt.zero_grad()
-    with autocast(enabled=False):
+    with autocast():
         enc_output = net.transmitter(X, valid_lens)
         channel_output = net.channel.add_AWGN(enc_output, 12)
         # print(check_snr(enc_output, channel_output))
